ce9f8ad455d9cb61c4fca5d31ced2ec77fdb70cb
Diogo Reis <diogoreis133@gmail.com>
2021-04-22 17:42:54 +0100

    inicio da proxy

---
+++ proxy.py
@@ -0,0 +1,20 @@
+import xmlrpc.server
+
+from flask import Flask, render_template
+
+from xmlrpc import client
+
+
+app = Flask(__name__)
+
+dbGPS = client.ServerProxy("http://127.0.0.1:8000/apiGPSLog")
+dbWiFi = client.ServerProxy("http://127.0.0.1:8001/apiWiFiLog")
+dbUser = client.ServerProxy("http://127.0.0.1:8002/apiUser")
+
+# ENDPOINTS
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 3002, debug = True)
