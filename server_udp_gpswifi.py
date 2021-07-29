a7236981f30997ec3c82c3780f5dbdf95a10a0f1
Diogo Reis <diogoreis133@gmail.com>
2021-07-29 18:40:00 +0100

    altitude in server gpswifi

--- server_udp_gpswifi.py
+++ server_udp_gpswifi.py
@@ -47,6 +47,7 @@ while(True):
 
     if message_s[0] == "GPS":
         number = dbGPS.newGPSLog(id_user, message_s[2], message_s[3], float(message_s[4]), float(message_s[5]))
+        print("GPS Location" + message_s[4]  + " " + message_s[5] + " "+ message_s[6])
         print(number)
         print("")
     elif message_s[0] == "WiFi":
